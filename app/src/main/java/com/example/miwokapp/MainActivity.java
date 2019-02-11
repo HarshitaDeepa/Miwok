a105608b69a6dc7f6fde239a7f7cd81b52f775ed
Harshita Aggarwal <harshita@Harshita-MacBook-Air.local>
2019-02-11 18:49:17 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/miwokapp/MainActivity.java
@@ -0,0 +1,15 @@
+ckage com.example.miwokapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.miwokapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
